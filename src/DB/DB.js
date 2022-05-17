23e18bce9141c7247247d8bd92ec55b7fd267460
noamTzuber <noamtooby@gmail.com>
2022-05-17 12:33:10 +0300

    change database

--- src/DB/DB.js
+++ src/DB/DB.js
@@ -1,12 +1,11 @@
 
-
-
 export const users = [{
     id: 'harry',
     name: 'potter',
     password: 'harry1234',
     server: "localhost1234",
     profilePic: "harry.jpg",
+
     contacts: [{
         idc: 'Malfoy',
         name: 'Malfoy',
@@ -25,98 +24,5 @@ export const users = [{
             lastdate: '18:32 13.04.22',
         }]
 
-    // chats: [
-    //     {
-    //         idc: 'Malfoy',
-    //         name: 'Malfoy',
-    //         img: "malfoy.jpg",
-    //         last: 'no way!',
-    //         lastdate: '18:32 13.04.22',
-    //         text: [{content: 'Hey Potter! Want to join Slytherin?', sent: true, created: "18:29 13.04.22"}]
-    //
-    //
-    //
-    //
-    //     },
-    //     {
-    //     idc: 'Ron',
-    //                 name: 'Ron',
-    //                 img: "ron.jpg",
-    //                 last: 'no way!',
-    //                 lastdate: '18:32 13.04.22',
-    //                 text: [{content: 'Hey Ron! Want to join Ron?', sent: true, created: "18:29 13.04.22"}]
-    //     }]}
 }]
-//         ,
-//         {
-//             idc: 'Hermione',
-//             name: 'Hermione',
-//             img: "hermione.jpg",
-//             last: 'image',
-//             lastdate: '16:43 13.04.22',
-//             text: [{content: 'Does the scar hurt you again?', sent: true, created: "16:29 13.04.22"}
-//             ]
-//         },
-//         {
-//             idc: 'Ron',
-//             name: 'Weasley',
-//             img: "ron.jpg",
-//             last: 'audio',
-//             lastdate: '13:56 13.04.22',
-//             text: [{content: 'We need to get 100 in the test!', sent: true, created: "13:14 13.04.22"}
-//             ]
-//         },
-//         {
-//             idc: 'Dumbledore',
-//             name: 'Dumbush',
-//             img: "dumbledore.jpg",
-//             last: 'You are a wizard Harry!',
-//             lastdate: '10:24 13.04.22',
-//             text: [{content: 'You are a wizard Harry!', sent: true, created: "10:24 13.04.22"}]
-//         },
-//         {
-//             idc: 'Voldemort',
-//             name: 'Tom',
-//             img: "voldemort.jpeg",
-//             last: 'video',
-//             lastdate: '9:02 13.04.22',
-//             text: [{content: 'I come for you...', sent: true, created: "9:01 13.04.22"}]
-//         },
-//     ]
-// },{
-//     id: 'noam',
-//     name: 'Noamos',
-//     password: 'noam1234',
-//     profilePic: "ron.jpg",
-//     chats: [
-//     {
-//         idc: 'harry',
-//         name: 'potter',
-//         img: "harry.jpg",
-//         last: 'Hey my name is noam',
-//         lastdate: '20:29 14.04.22',
-//         text: [{content: 'Hey my name is noam', sent: false, created: "20:29 14.04.22"}]
-//     },
-//
-// ]
-// },
-//         {
-//         id: 'itay',
-//         name: 'itayos',
-//         password: 'itay1234',
-//         profilePic: "dobby.jpg",
-//         chats: [
-//             {
-//                 contact: 'harry',
-//                 name: 'potter',
-//                 img: "harry.jpg",
-//                 last: 'Hey my name is noam',
-//                 lastdate: '20:29 14.04.22',
-//                 text: [{content: 'Hey my name is noam', sent: false, created: "20:29 14.04.22"}]
-//             },
-//
-//         ]
-//     }
-//
-// ]
 export default users
