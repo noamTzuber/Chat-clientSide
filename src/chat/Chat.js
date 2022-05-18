2c5b86f7bb30e4284c55c4b14a5069c731d5f671
itayshwartz1 <itay2007@gmail.com>
2022-05-18 14:22:58 +0300

    s

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -15,16 +15,62 @@ import { useLocation } from 'react-router-dom';
 
 
 function Chat() {
+
     const { state } = useLocation();
     const { id } = state;
+    
+    const response = fetch("https://localhost:1234/api/Contact/AllUsers")
+    const data = response.data;
+    console.log(data);
 
+
+    // const [myChats, setMyChats] = useState([]);
+    // useEffect(async () =>{
+    //     const res = await fetch("https://localhost:1234/api/Contact/Chats");
+    //     const data = await res.json();
+    //     setMyChats(data);
+        
+    // },[])
+    
+
+
+    // const [myUser, setMyUser] = useState({ id: '', name: '', password: '', server: '', contacts: [] });
+    // useEffect(async () =>{
+    //     const res = await fetch("https://localhost:1234/api/Contact/User");
+    //     const data = await res.json();
+    //     setMyUser(data);
+    // },[])
+            // console.log(myUser.contacts);
+
+
+
+    // useEffect(async () =>{
+    //     await fetch("https://localhost:1234/api/Contact/User").then(response =>{
+    //         if(response.ok){
+    //             return response.json();
+    //         }
+    //     })
+    //     .then(data => {
+    //         setMyUser(data);
+    //     })
+    // },[])
+
+    // async function get(){
+    //     const res = await fetch("https://localhost:1234/api/Contact/User");
+    //     const data = await res.json();
+    //     setMyUser(data);
+    // }
+    // // get();
+    // console.log(myUser);
+
+
+    
 
     const [currentTalk, setCurrentTalk] = useState({ id: 0, user1: '', user2: '', Messages: [] });
     const [currentMessages, setCurrentMessages] = useState([]);
     const [contacts, setContacts] = useState(users[id].contacts);
 
-    const [user2, setUser2] = useState({ id: '', name: '', password: '', server: '', contacts: [] });
-    const [chats2, setChats2] = useState([])
+
 
    
     // async function post(){
@@ -36,19 +82,8 @@ function Chat() {
     //                 });
     
     // }
+    // console.log(myUser);
     
-    async function get(){
-        const response = await axios.get("https://localhost:1234/api/Contact/Chats");
-        // setChats2(response.data);
-         
-    }
-    
-    get();
-    console.log(chats2);
-
-    
-
-
     const userList = contacts.map((users, key) => {
         return < SummaryConversation{...users} setCurrentConversation={setCurrentTalk} id={id} num={key} key={key} />
     });
